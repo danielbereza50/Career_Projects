dbbcfab92aed2bb07035449b5aab1c3fe4393125
Daniel Bereza <danielbereza50@gmail.com>
2021-08-23 20:49:33 -0400

    Update template.sql

--- MySQL/template.sql
+++ MySQL/template.sql
@@ -1,8 +1,17 @@
-UPDATE wp_options SET option_value = replace(option_value, 'oldurl.com', 'newurl.com') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_options
+SET option_value = replace(option_value, 'oldurl.com', 'newurl.com') 
+WHERE option_name = 'home' 
+OR option_name = 'siteurl';
 
-UPDATE wp_posts SET guid = replace(guid, 'oldurl.com','newurl.com');UPDATE wp_posts SET post_content = replace(post_content, 'oldurl.com', 'newurl.com');
+UPDATE wp_posts 
+SET guid = replace(guid, 'oldurl.com','newurl.com');
 
-UPDATE wp_postmeta SET meta_value = replace(meta_value,'oldurl.com','newurl.com');
+UPDATE wp_posts 
+SET post_content = replace(post_content, 'oldurl.com', 'newurl.com');
+
+UPDATE wp_postmeta 
+SET meta_value = replace(meta_value,'oldurl.com','newurl.com');
+
 
 INSERT INTO 'databasename'.'wp_users' ('ID', 'user_login', 'user_pass') 
 VALUES ('4', 'demo', MD5('demo'));
