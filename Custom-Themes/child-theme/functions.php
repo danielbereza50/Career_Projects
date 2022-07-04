7654436a1c787a677c9577f4acafe906eaa0145b
Daniel Bereza <daniel@212creative.com>
2022-07-03 23:46:11 -0400

    updates

--- Custom-Themes/child-theme/functions.php
+++ Custom-Themes/child-theme/functions.php
@@ -9,12 +9,12 @@ function divi__child_theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array('parent-style')
     );
-    $custom_css=filemtime(__DIR__.'/divi-style.min.css');
-    wp_enqueue_style( 'child-style_custom',get_stylesheet_directory_uri() . '/divi-style.min.css',NULL,$custom_css);
+    $custom_css=filemtime(__DIR__.'/parent-style.min.css');
+    wp_enqueue_style( 'child-style_custom',get_stylesheet_directory_uri() . '/parent-style.min.css',NULL,$custom_css);
 
     $custom_js=filemtime(__DIR__.'/custom-divi.js');
     wp_enqueue_script( 'custom-js','https://code.jquery.com/jquery-2.2.4.min.js',NULL,true );
-    wp_enqueue_script( 'custom-divi-js',get_stylesheet_directory_uri().'/custom-divi.js',array('jquery'), $custom_js, true);
+    wp_enqueue_script( 'custom-parent-js',get_stylesheet_directory_uri().'/custom-parent.js',array('jquery'), $custom_js, true);
 }
 add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
 //
