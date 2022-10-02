17bea266e5de67b4742bde055eb3001f2afbad47
Daniel Bereza <danielbereza50@gmail.com>
2022-10-01 22:15:24 -0400

    module and theme updates

--- Custom Modules/welcome_module/src/Controller/WelcomeController.php
+++ Custom Modules/welcome_module/src/Controller/WelcomeController.php
@@ -1,9 +1,27 @@
 <?php
+
 namespace Drupal\welcome_module\Controller;
+use Drupal\Core\Render\Markup;
+
 class WelcomeController {
   public function welcome() {
-    return array(
-      '#markup' => 'Welcome to our Website.'
-    );
+   
+    $header = [
+      'col1' => t('COL1'),
+      'col2' => t('COL2'),
+    ];
+  
+    $rows = [
+  	  [['data' => 'Welcome', 'class' => 'test-col-1'],'User'],
+  	  [['data' => 'Welcome', 'class' => 'test-col-2'],'User'],
+  	  [['data' => 'Welcome', 'class' => 'test-col-3'],'User'],
+	  ];
+
+    return [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+
   }
 }
