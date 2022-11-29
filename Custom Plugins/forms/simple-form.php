c1d9030d87c9c6036fce366d3ad7679dddf37960
Daniel Bereza <danielbereza50@gmail.com>
2022-11-29 16:10:56 -0500

    Update simple-form.php

--- Custom Plugins/forms/simple-form.php
+++ Custom Plugins/forms/simple-form.php
@@ -26,7 +26,7 @@
 							   "Hear About:\n $hear_out.\n".
 							   "Message:\n $message.\n".
 				
-				$to = "danielbereza50@gmail.com";
+				$to = "noreply@gmail.com";
 	  			$headers = "From: $email_from \r\n";
 	 			$headers .= "Reply-To: $email \r\n";
 	  			mail($to,$email_subject,$email_body,$headers);
