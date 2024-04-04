9a2f330f08cc8b284cf4520e29ff84b52a789b10
Daniel Bereza <daniel@212creative.com>
2024-04-04 16:09:49 -0400

    upload test

---
+++ wp-content/themes/Divi-Child-Theme/custom-divi.js
@@ -0,0 +1,16 @@
+// A $( document ).ready() block.
+jQuery( document ).ready(function() {
+
+	//var inputList = document.querySelectorAll("[id^='patientName']");
+    
+    console.log( "ready!" );
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
